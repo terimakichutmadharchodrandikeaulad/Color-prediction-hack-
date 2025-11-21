b3629f39ee8df3b41652a830485806dd84dcc71d
terimakichutmadharchodrandikeaulad <163394065+terimakichutmadharchodrandikeaulad@users.noreply.github.com>
2025-11-21 13:38:21 +0530

    Add files via upload

---
+++ js/inline_scripts.js
@@ -0,0 +1,10 @@
+window.CONFIG=Object.freeze({"VITE_API_URL":"https://api.bdg88zf.com","VITE_BASE_LANGUAGE":"en","VITE_BASE_DOLLARSIGN":"₹","VITE_BASE_LANGUAGES":"en","VITE_BASE_PROJECTNAME":"bdgwin","VITE_BASE_CURRENCY":"INR","VITE_PNGSTART":"bdgwin","VITE_HOME":"blueHome","VITE_MAINCOLOR":"blackGoldStyle","VITE_GTAG_ID":"G-JJ7G51SZ42","BASE_URL":"/","MODE":"production","DEV":true,"PROD":false});
+
+/* ===== INLINE JAVASCRIPT ===== */
+
+
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){ dataLayer.push(arguments); }
+          gtag('js', new Date());
+          gtag('config', 'G-JJ7G51SZ42');
+        
